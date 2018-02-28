6d998f213135fe49de6e66208e12c2d0a99825c6
Chris Watkin <chriswatkin@gmail.com>
2018-02-27 17:24:21 -0700

    add CRUD code/html

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'people/index'
+  root 'people#index'
+  
+  resources :people
+  #root 'static_pages#home'
+  #get '/about', to: 'static_pagess#about'
+  
 end
